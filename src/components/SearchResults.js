536b02b42cbb6bb8f140ba7a48b9c713a83a3767
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 11:05:11 -0500

    Receive handleAddToUserPlayList in src/components/SearchResults.js and pass it to Tracklist component

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Tracklist from './Tracklist';
 
-function SearchResults() {
+function SearchResults({results, handleAddToUserPlayList}) {
     return (
         <div className='results'>
             <h1>Search Results</h1>
+            <Tracklist tracks={results} handleAddToUserPlayList={handleAddToUserPlayList} />
         </div>
     )
 }
