797bda1f2a2e2bef87f7edc51da07b0abf52f552
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 10:29:18 -0500

    Add exampleTracks in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import SearchBar from "./components/SearchBar";
 
+const exampleTracks = [
+  {
+    artist: 'Halestorm',
+    title: 'Painkiller'
+  },
+  {
+    artist: '5 Doors Down',
+    title: 'Kryptonite'
+  },
+  {
+    artist: 'Nirvana',
+    title: 'Smells Like Teen Spirit'
+  },
+  {
+    artist: 'John Williams',
+    title: 'Imperial March'
+  }
+]
+
 function App() {
   return (
     <div className="App">
