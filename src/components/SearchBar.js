7ca421ff31e18f55f7541f9fdd0dc4a012f4cc8c
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 09:58:53 -0500

    Add basic form to components/SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function SearchBar() {
-
+    return (
+        <form>
+            <input type='text' placeholder='Search for a song...' />
+            <button>Search</button>
+        </form>
+    )
 }
 
 export default SearchBar;
