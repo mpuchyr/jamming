f1afded3e5d7f53d078fe2f23505d0e4d5aa6bde
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 10:51:24 -0500

    Update Track.js to accept children props

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Track({track}) {
+function Track({track, children}) {
     return (
         <div className='track'>
             <h1>{track.title}</h1>
             <h2>{track.artist}</h2>
+            {children}
         </div>
     )
 }
