cb8d724c4c8d37e56df05b471deddcecb5373d3d
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 11:03:43 -0500

    Receive handleAddToUserPlayList as prop in Tracklist.js

--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Track from './Track';
 
-function Tracklist({tracks}) {
+function Tracklist({tracks, handleAddToUserPlayList}) {
     return (
         <div className='track-list'>
             <h1>Tracklist</h1>
-            {tracks.map((track) => <Track track={track} key={track.title} />)}
+            {tracks.map((track) => <Track track={track} key={track.title}><button onClick={() => handleAddToUserPlayList(track)}>+</button></Track>)}
         </div>
     )
 }
