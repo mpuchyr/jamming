6976d047adff92ac5f68c6a76e3c97ad435f2394
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 11:04:18 -0500

    Update src/components/Playlist.js to display tracks properly

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Track from './Track';
 
-function Playlist() {
+function Playlist({tracks}) {
     return (
         <div className='play-list'>
             <h1>Play List</h1>
+            {tracks.map((track) => <Track track={track}/>)}
+            
         </div>
     )
 }
