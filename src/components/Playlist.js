70c70e086ff4145749055b5441f53cbb999e296a
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 11:53:59 -0500

    Add functionality to edit playlist name by clicking on it in src/components/Playlist.js

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Track from './Track';
 
 function Playlist({tracks, handleRemoveFromUserPlayList, playListTitle, setPlayListTitle}) {
+    const [editingTitle, setEditingTitle] = useState(false);
+
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') {
+            setEditingTitle(false);
+        }
+    }
+
     return (
         <div className='play-list'>
-            <input value={playListTitle} type='text' onChange={(e) => setPlayListTitle(e.target.value)}/>
+            {!editingTitle && <h1 onClick={() => setEditingTitle(true)}>{playListTitle}</h1>}
+            {editingTitle && <input value={playListTitle} type='text' onChange={(e) => setPlayListTitle(e.target.value)} onKeyDown={(e) => handleKeyDown(e)}/>}
             {tracks.map((track) => <Track track={track}><button onClick={() => handleRemoveFromUserPlayList(track.id)}>-</button></Track>)}
             <button>Save to Spotify</button>
         </div>
