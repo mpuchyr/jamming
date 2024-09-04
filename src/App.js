74f747a819220401d4f231a66c0d4d76d6ca215c
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 10:57:20 -0500

    Create function handleAddToUserPlayList, import useState, add ids to exampleTracks in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
 import Playlist from "./components/Playlist";
@@ -5,27 +6,39 @@ import Playlist from "./components/Playlist";
 const exampleTracks = [
   {
     artist: 'Halestorm',
-    title: 'Painkiller'
+    title: 'Painkiller',
+    id: 1
   },
   {
     artist: '5 Doors Down',
-    title: 'Kryptonite'
+    title: 'Kryptonite',
+    id: 2
   },
   {
     artist: 'Nirvana',
-    title: 'Smells Like Teen Spirit'
+    title: 'Smells Like Teen Spirit',
+    id: 3
   },
   {
     artist: 'John Williams',
-    title: 'Imperial March'
+    title: 'Imperial March',
+    id: 4
   }
 ]
 
+
 function App() {
+  const [results, setResults] = useState(exampleTracks);
+  const [userPlayList, setUserPlayList] = useState([]);
+
+  function handleAddToUserPlayList(newTrack) {
+    setUserPlayList(prevPlayList => [...prevPlayList, newTrack]);
+  }
+
   return (
     <div className="App">
       <SearchBar />
-      <SearchResults results={exampleTracks} />
+      <SearchResults results={results} />
       <Playlist />
     </div>
   );
