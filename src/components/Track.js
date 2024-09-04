447f4d5cedc82ae1e5dd0dd8f771b7444bab8ff8
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 10:47:58 -0500

    Update Tracklist.js and Track.js to accept and display tracks

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Track() {
+function Track({track}) {
     return (
         <div className='track'>
-            <h1>Song Name</h1>
-            <h2>Artist</h2>
+            <h1>{track.title}</h1>
+            <h2>{track.artist}</h2>
         </div>
     )
 }
--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Track from './Track';
 
-function Tracklist() {
+function Tracklist({tracks}) {
     return (
         <div className='track-list'>
             <h1>Tracklist</h1>
+            {tracks.map((track) => <Track track={track} key={track.title} />)}
         </div>
     )
 }
