ca1e87b5ff4520f014a7f9cc5adc6ac9a4fed502
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-04 13:02:06 -0500

    Add functionality to retain the search in state in SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar() {
+    const [searchInput, setSearchInput] = useState('');
+
+    function handleOnChange(e) {
+        setSearchInput(e.target.value);
+    }
+
+    function handleOnSubmit(e) {
+        e.preventDefault();
+        console.log(e);
+        setSearchInput('');
+    }
+
     return (
-        <form>
-            <input type='text' placeholder='Search for a song...' />
+        <form onSubmit={handleOnSubmit}>
+            <input type='text' placeholder='Search for a song...' value={searchInput} onChange={(e) => handleOnChange(e)}/>
             <button>Search</button>
         </form>
     )
