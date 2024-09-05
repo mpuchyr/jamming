0d9f08fa85dad5a87b54cdddf645394a3c577a04
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2024-09-05 12:59:42 -0500

    Remove Login.js and update App.js to reflect the change

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react';
 import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
 import Playlist from "./components/Playlist";
+import Spotify from './spotify';
+
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+const redirect_url = 'http://localhost:3000/'
 
 function App() {
   const [accessToken, setAccessToken] = useState('');
@@ -12,20 +15,24 @@ function App() {
   const [userPlayList, setUserPlayList] = useState([]);
   const [playListTitle, setPlayListTitle] = useState('My Playlist');
 
-  useEffect(() => {
-    // API Access Token
-    let authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
-      },
-      body: 'grant_type=client_credentials'
-    }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result => result.json())
-      .then(data => setAccessToken(data.access_token))
+  // useEffect(() => {
+  //   // API Access Token
+  //   let authParameters = {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //       'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
+  //     },
+  //     body: 'grant_type=client_credentials'
+  //   }
+  //   fetch('https://accounts.spotify.com/api/token', authParameters)
+  //     .then(result => result.json())
+  //     .then(data => setAccessToken(data.access_token))
 
+  // }, [])
+
+  useEffect(() => {
+    Spotify.getAccessToken();
   }, [])
 
   async function handleSearch(searchInput) {
@@ -38,6 +45,18 @@ function App() {
     const data = await response.json();
     setResults(data.tracks.items);
   }
+
+  async function getUsername() {
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    })
+    const data = await response.json();
+    console.log(data);
+  }
+
     
 
   function handleAddToUserPlayList(newTrack) {
